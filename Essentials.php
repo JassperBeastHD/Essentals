edeb502c53c45e5d512567909514d1a939cb7540
JassperBeastHD <JassperBeastHD@users.noreply.github.com>
2014-02-10 00:33:19 -1000

    Bitch.php
    Bitch mode

--- Essentials.php
+++ Essentials.php
@@ -23,203 +23,15 @@ class Essentials implements Plugin{
 	}
 	
 	public function init(){
-		$this->api->event("server.close", array($this, "handler"));
-		$this->api->addHandler("player.join", array($this, "handler"), 5);
-		$this->api->addHandler("player.quit", array($this, "handler"), 5);
-		$this->api->addHandler("player.chat", array($this, "handler"), 5);
-		$this->api->addHandler("player.move", array($this, "handler"), 5);
-		$this->api->addHandler("player.spawn", array($this, "handler"), 5);
-		$this->api->addHandler("console.check", array($this, "handler"), 5);
-		$this->api->addHandler("player.teleport", array($this, "handler"), 5);
-		$this->api->addHandler("player.respawn", array($this, "handler"), 5);
-		$this->api->addHandler("console.command", array($this, "handler"), 5);
-		
-		$this->api->console->register("afk", "", array($this, "defaultCommands"));
-		$this->api->console->register("home", "<name>", array($this, "defaultCommands"));
-		$this->api->console->register("sethome", "<name>", array($this, "defaultCommands"));
-		$this->api->console->register("delhome", "<home>", array($this, "defaultCommands"));
-		$this->api->console->register("mute", "<player>", array($this, "defaultCommands"));
-		$this->api->console->register("back", "", array($this, "defaultCommands"));
-		$this->api->console->register("tree", "<tree|brich|redwood>", array($this, "defaultCommands"));
-		$this->api->console->register("setspawn", "", array($this, "defaultCommands"));
-		$this->api->console->register("burn", "<player> <seconds>", array($this, "defaultCommands"));
-		$this->api->console->register("kickall", "[reason]", array($this, "defaultCommands"));
-		$this->api->console->register("killall", "[reason]", array($this, "defaultCommands"));
-		$this->api->console->register("heal", "[player]", array($this, "defaultCommands"));
 		$this->api->console->register("clearinventory", "[player] [item]", array($this, "defaultCommands"));
-		$this->readConfig();
+		
 		
 		console("[INFO] Essentials enabled!");
-		$this->api->schedule(20, array($this, "checkAFK"), array(), true);
-	}
+		}
 	
 	public function __destruct(){}
 	
-	public function readConfig(){
-		$this->path = $this->api->plugin->createConfig($this, array(
-			"login" => array(
-				"timeout" => 60,
-				"allow-register" => true,
-				"allow-chat" => false,
-				"allow-commands" => array(),
-				"allow-move" => false,
-				"pw-higher-then" => 4,
-				"pw-less-then" => 15,
-				"login-kick" => 5,
-			),
-			"chat-format" => "<{DISPLAYNAME}> {MESSAGE}",
-			"blacklist" => array(
-				"placement" => '',
-				"usage" => '',
-				"break" => '',
-			),
-			"kits" => array(),
-			"auto-afk" => 300,
-			"auto-afk-kick" => -1,
-			"freeze-afk-players" => false,
-			"newbies" => array(
-				"kit" => "",
-				"message" => "Welcome {DISPLAYNAME} to the server!",
-			),
-			"creative-item" => array(
-				"op" => array(),
-				"default" => array(),
-			),
-			"player-commands" => array(),
-		));
-		if(is_file($this->path."messages.yml")){
-			$this->lang = new Config($this->path."messages.yml", CONFIG_YAML);
-		}else{
-			console("[ERROR] \"messages.yml\" file not found!");
-		}
-		if(is_dir(DATA_PATH."/plugins/Essentials/userdata/") === false){
-			mkdir(DATA_PATH."/plugins/Essentials/userdata/");
-		}
-		
-		$this->motd = $this->server->motd;
-		$this->server->motd = "";
-		$this->config = $this->api->plugin->readYAML($this->path."config.yml");
-	}
 	
-	public function checkAFK(){
-		foreach($this->lastafk as $iusername => $time){
-			$player = $this->api->player->get($iusername, false);
-			if((time() - $time) == $this->config["auto-afk"]){
-				$this->api->chat->broadcast($this->getMessage("userIsAway", array($player->username, "", "", "")));
-				$this->afk[$player->iusername] = true;
-			}
-			if((time() - $time) == $this->config["auto-afk-kick"]){
-				//$this->api->ban->kick($iusername, "Auto AFK kick");
-				$player->close("Auto AFK kick");
-			}
-		}
-	}
-	
-	public function handler(&$data, $event){
-		switch($event){
-			case "player.join":
-					$this->afk[$data->iusername] = false;
-					$this->data[$data->iusername] = new Config(DATA_PATH."/plugins/Essentials/userdata/".$data->iusername.".yml", CONFIG_YAML, array(
-						"ipAddress" => $data->ip,
-						"mute" => false,
-						"newbie" => true,
-					));
-				break;
-			case "player.quit":
-				if($this->data[$data->iusername] instanceof Config){
-					$this->data[$data->iusername]->save();
-				}
-				unset($this->lastafk[$data->iusername]);
-				unset($this->afk[$data->iusername]);
-				break;
-			case "player.respawn":
-				$data->sendChat($this->getMessage("backAfterDeath"));
-				break;
-			case "player.teleport":
-				$this->setData($data["player"], "lastlocation", array(
-					"world" => $data["player"]->level->getName(),
-					"x" => $data["player"]->entity->x,
-					"y" => $data["player"]->entity->y,
-					"z" => $data["player"]->entity->z,
-				));
-				break;
-			case "player.chat":
-				$message = str_replace(array("{DISPLAYNAME}", "{MESSAGE}", "{WORLDNAME}"), array($data["player"]->username, $data["message"], $data["player"]->level->getName()), $this->config["chat-format"]);
-				$this->api->chat->broadcast($message);
-				return false;
-				break;
-			case "player.move":
-				if(EssentialsAPI::checkEssentialsLogin() and EssentialsAPI::checkLogin($data->player) === false){
-					break;
-				}
-				$this->lastafk[$data->player->iusername] = time();
-				if($this->afk[$data->player->iusername]){
-					$this->afk[$data->player->iusername] = false;
-					$this->api->chat->broadcast($this->getMessage("userIsNotAway", array($data->player->username, "", "", "")));
-				}
-				break;
-			case "console.check":
-				if(in_array($data["cmd"], $this->config["player-commands"]) or $this->api->ban->isOp($data["issuer"]->username)){
-					return true;
-				}
-				break;
-			case "console.command":
-				if($data["issuer"] instanceof Player and $this->api->dhandle("get.player.permission", "") !== false){
-					if(EssentialsAPI::checkEssentialsLogin() and EssentialsAPI::checkLogin($data["issuer"]) === false){
-						break;
-					}
-					if(in_array($data["cmd"], $this->config["player-commands"]) or $this->api->ban->isOp($data["issuer"]->username)){
-						return;
-					}
-					return false;
-				}
-				break;
-			case "player.spawn":
-				if($this->getData($data, "newbie")){
-					switch($data->gamemode){
-						case SURVIVAL:
-							if(!array_key_exists($this->config["newbies"]["kit"], $this->config["kits"])){
-								break;
-							}
-							$kits = $this->config["kits"][$this->config["newbies"]["kit"]];
-							foreach($kits as $kit){
-								$kit = explode(" ", $kit);
-								$item = BlockAPI::fromString(array_shift($kit));
-								$count = $kit[0];
-								$data->addItem($item->getID(), $item->getMetadata(), $count);
-							}
-							break;
-						case CREATIVE:
-							break;
-					}
-					$data->sendChat(str_replace(array("{DISPLAYNAME}", "{WORLDNAME}"), array($data->username, $data->level->getName()), $this->config["newbies"]["message"]));
-					$this->setData($data, "newbie", false);
-				}else{
-					$data->sendChat($this->motd);
-				}
-				if($data->gamemode === CREATIVE){
-					$type = $this->api->ban->isOp($data->iusername) ? "op" : "default";
-					$creative = $this->config["creative-item"][$type];
-					foreach($creative as $item){
-						$item = explode(" ", $item);
-						$data->setSlot($item[0], BlockAPI::fromString($item[1]));
-					}
-				}
-				break;
-		}
-	}
-	
-	public function getData(Player $player, $dataname){
-		return $this->data[$player->iusername]->exists($dataname) ? $this->data[$player->iusername]->get($dataname) : false;
-	}
-	
-	public function setData(Player $player, $dataname, $data){
-		$this->data[$player->iusername]->set($dataname, $data);
-	}
-	
-	public function delData(Player $player, $dataname){
-		$this->data[$player->iusername]->remove($dataname);
-	}
 	
 	public function defaultCommands($cmd, $params, $issuer, $alias){
 		$output = "";
@@ -411,7 +223,7 @@ class Essentials implements Plugin{
 				if($params[0] != ""){
 					$target = $this->api->player->get($params[0]);
 					if($target === false){
-						$output .= $this->getMessage("playerNotFound");
+						$output .= "playerNotFound";
 						break;
 					}
 				}
@@ -428,60 +240,11 @@ class Essentials implements Plugin{
 					}
 					$issuer->setSlot($slot, BlockAPI::getItem(AIR, 0, 0));
 				}
-				$output .= $params[0] == "" ? $this->getMessage("inventoryCleared") : $this->getMessage("inventoryClearedOthers", array($target->username, "", "", ""));
+				$output .= "Inventory cleared.";
 				break;
 		}
 		return $output;
 	}
 	
-	public function getMessage($msg, $params = array("%1", "%2", "%3", "%4")){
-		$msgs = array_merge($this->lang->get("Default"), $this->lang->get("Essentials"), $this->lang->get("Login"), $this->lang->get("Protect"));
-		if(!isset($msgs[$msg])){
-			$msgs[$msg] = "Undefined message: $msg";
-		}
-		return str_replace(array("%1", "%2", "%3", "%4"), array($params[0], $params[1], $params[2], $params[3]), $msgs[$msg])."\n";
-	}
-}
-
-class EssentialsAPI{
-	private static $essentials, $essentialsLogin, $essentialsProtect;
-	public static function setEssentials(Essentials $plugin){
-		if(EssentialsAPI::$essentials instanceof Essentials){
-			return false;
-		}
-		EssentialsAPI::$essentials = $plugin;
-	}
-	
-	public static function setEssentialsLogin(EssentialsLogin $plugin){
-		if(EssentialsAPI::$essentialsLogin instanceof EssentialsLogin){
-			return false;
-		}
-		EssentialsAPI::$essentialsLogin = $plugin;
-	}
-	
-	public static function setEssentialsProtect(EssentialsProtect $plugin){
-		if(EssentialsAPI::$essentialsProtect instanceof EssentialsProtect){
-			return false;
-		}
-		EssentialsAPI::$essentialsProtect = $plugin;
-	}
-	
-	public static function getPlayerData(Player $player, $dataname){
-		if(in_array($dataname, array("home", "lastlocation", "mute", "newbie"))) return EssentialsAPI::$essentials->getData($player, $dataname);
-		if(in_array($dataname, array("status", "registered", "lastconnected", "forget"))) return EssentialsAPI::$essentialsLogin->getData($player, $dataname);
-		return false;
-	}
-	
-	public static function getMessage($msg, $params = array("%1", "%2", "%3", "%4")){
-		return EssentialsAPI::$essentials->getMessage($msg, $params);
-	}
-	
-	public static function checkEssentialsLogin(){
-		if(!(EssentialsAPI::$essentialsLogin instanceof EssentialsLogin)) return false;
-		return true;
-	}
-	
-	public static function checkLogin(Player $player){
-		return EssentialsAPI::$essentialsLogin->getData($player, "status") == "login" ? true : false;
 	}
 }
